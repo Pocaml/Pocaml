15178dc0429c496ea75748dc91a06142d8e2d043
Peter Choi <jc4883@columbia.edu>
2021-11-11 15:59:18 -0500

    Add initial test cases for def

--- lib/print.ml
+++ lib/print.ml
@@ -10,21 +10,53 @@ let string_of_typ = function
 let string_of_param = function
   | ParamAnn(var_id, typ) -> 
     let typ_string = string_of_typ typ in
-    if typ_string == "" then var_id else
+    if typ_string = "" then var_id else
       "( " ^ var_id ^ " : " ^ typ_string ^ " )"
 
 let string_of_params = function
   params -> String.concat " " (List.map string_of_param params)
 
+let string_of_lit = function
+  LitInt(int) -> string_of_int int
+  | _ -> "some other lit"
+  (* | LitString of string
+  | LitChar of char
+  | LitList of expr list
+  | LitBool of bool *)
+
+let string_of_expr = function
+    Lit(literal) -> string_of_lit literal
+  | _ -> "some other expr"
+  (* | Var of var_id
+  | UnaryOp of unary_op * expr
+  | BinaryOp of expr * binary_op * expr
+  | Conditional of expr * expr * expr
+  | Letin of var_id * expr * expr
+  | Lambda of param list * expr
+  | Apply of expr * expr
+  | Match of expr * (pat * expr) list *)
 
 let string_of_def = function
-      (* Def(var_id, params_opt, typ, expr) -> "Def" *)
-      Def(_, _, _, _) -> "Def"
-    | DefRecFn(_, _, _, _) -> "DefRecFn"
+      Def(var_id, params_opt, typ, expr) ->
+      let type_str = string_of_typ typ in
+      let formatted_type_str = if type_str = "" then "" else (" : " ^ type_str) in
+      let params_str = string_of_params params_opt in
+      let formatted_params_str = if params_str = "" then "" else " " ^ params_str in
+      "let " ^ var_id ^ formatted_params_str ^ formatted_type_str ^ " = " ^ string_of_expr expr
+    | DefRecFn(var_id, params_opt, typ, expr) ->
+      let type_str = string_of_typ typ in
+      let formatted_type_str = if type_str = "" then "" else (" : " ^ type_str) in
+      let params_str = string_of_params params_opt in
+      let formatted_params_str = if params_str = "" then "" else " " ^ params_str in
+      "let rec " ^ var_id ^ formatted_params_str ^ formatted_type_str ^ " = " ^ string_of_expr expr
 
 let string_of_program = function
     Program(defs) -> String.concat "\n" (List.map string_of_def defs)
 
+let print_prog = function
+    str -> let lexbuf = Lexing.from_string str in
+    let prog = Parser.program (Lexer.token) lexbuf in
+    print_endline (string_of_program prog)
 
 let _ =
   let lexbuf = Lexing.from_channel stdin in
--- test/ast/def.ml
+++ test/ast/def.ml
@@ -1,8 +1,25 @@
 open Pocaml.Print
-
+(* open Pocaml.Parser *)
+(* open Pocaml.Lexer *)
 
 
 let%expect_test _ =
-  string_of_def "let a = 3";
-  [%expect{||}]
+  print_prog "let fn (a: int) : int = 3";
+  [%expect{| let fn ( a : int ) : int = 3 |}]
+
+let%expect_test _ =
+  print_prog "let fn (a: int) = 3";
+  [%expect{| let fn ( a : int ) = 3 |}]
+
+let%expect_test _ =
+  print_prog "let rec fn (a: int): int = 3";
+  [%expect{| let rec fn ( a : int ) : int = 3 |}]
+
+let%expect_test _ =
+  print_prog "let rec fn (a: int) b (c: int): int = 3";
+  [%expect{| let rec fn ( a : int ) b ( c : int ) : int = 3 |}]
+
+let%expect_test _ =
+  print_prog "let a: int = 3";
+  [%expect{| let a : int = 3 |}]
 
