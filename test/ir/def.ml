fa975547d066ef6aceff1c79ad8860ff82d3c8aa
yiming-fang <yf2484@columbia.edu>
2021-11-18 23:19:42 -0500

    add tests for ir pretty print with promoted output

--- test/ir/def.ml
+++ test/ir/def.ml
@@ -1,8 +1,40 @@
-open Pocaml.Print
+open Pocaml.Print_ir
 
 (* open Pocaml.Parser *)
 (* open Pocaml.Lexer *)
 
 let%expect_test _ =
-  print_prog "let a: () = ()";
-  [%expect {| let a : () = Not Implemented |}]
+  print_prog "let fn (a: int) : int = 3";
+  [%expect {| let fn = ( fun ( a : int -> int ) = ( 3 : int ) ) |}]
+
+let%expect_test _ =
+  print_prog "let fn (a: int) = 3";
+  [%expect {| let fn = ( fun ( a : int ->  ) = 3 ) |}]
+
+let%expect_test _ =
+  print_prog "let rec fn (a: int): int = 3";
+  [%expect {| let fn = ( fun ( a : int -> int ) = ( 3 : int ) ) |}]
+
+let%expect_test _ =
+  print_prog "let rec fn (a: int) b (c: int): int = 3";
+  [%expect {| let fn = ( fun ( a : int ->  -> int -> int ) = ( fun ( b :  -> int -> int ) = ( fun ( c : int -> int ) = ( 3 : int ) ) ) ) |}]
+
+let%expect_test _ =
+  print_prog "let a: int = 3";
+  [%expect {| let a = ( 3 : int ) |}]
+
+let%expect_test _ =
+  print_prog "let a = \"some string\"";
+  [%expect {| let a = ['s';'o';'m';'e';' ';'s';'t';'r';'i';'n';'g'] |}]
+
+let%expect_test _ =
+  print_prog "let a = false";
+  [%expect {| let a = false |}]
+
+let%expect_test _ =
+  print_prog "let a = 'c'";
+  [%expect {| let a = 'c' |}]
+
+let%expect_test _ =
+  print_prog "let a = [1;2;3;]";
+  [%expect {| let a = [1;2;3] |}]
