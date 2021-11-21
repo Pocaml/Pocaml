887ce31310b657e79be65eab0a9157e896a39b4e
yunlanli <chnsidney@gmail.com>
2021-11-21 01:07:50 -0500

    builtin function for + operator

---
+++ builtins/add.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "builtins.h"
+
+
+void *_builtin_add(void **args)
+{
+	_pml_int *left_operand, *right_operand;
+	_pml_int *res = (_pml_int *) malloc(sizeof(_pml_int));
+
+	left_operand = (_pml_int *) args[0];
+	right_operand = (_pml_int *) args[1];
+
+	*res = *left_operand + *right_operand;
+	return (_pml_val) res;
+}
