a7267c469fbc4e30f08173af91766e1c8abcd400
Leo Qiao <qiaofeitong@hotmail.com>
2021-12-08 18:38:40 -0500

    add pocamlc '-t' option to typecheck with ocamlc

---
+++ builtins/builtins.ml
@@ -0,0 +1,6 @@
+let string_of_int : int -> string = fun _ -> ""
+let string_of_char : char -> string = fun _ -> ""
+let string_of_bool : bool -> string = fun _ -> ""
+let string_of_ : int -> string = fun _ -> ""
+let string_of_int : int -> string = fun _ -> ""
+let print_string : string -> unit = fun _ -> ()
